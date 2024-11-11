c28e12ab2be5eeddf245f1f6564bbca180e73a3d
Your Name <you@example.com>
2024-11-11 23:53:57 +0300

    almost done AVLtree

--- include/AVL_tree.h
+++ include/AVL_tree.h
@@ -4,10 +4,88 @@
 struct Node{
     int key;
     int bal;
-    Node* l, r;
+    Node* l, *r;
+};
+
+void LLturn(Node* root, bool operation){ //operation = true - insert, false - delete
+    Node* buf;
+    int tempBal;
+    buf = root->l;
+    root->l = buf->r;
+    buf->r = root;
+    if(operation){
+        root->bal = 0;
+        buf->bal = 0;
+    }
+    else{
+        tempBal = buf->bal;
+        if (tempBal == 0){
+            root->bal = -1;
+            buf->bal = 1;
+        }else{
+            root->bal = 0;
+            buf->bal = 0;
+        }
+    }
+    root = buf;
 }
 
+void LRturn(Node* root, bool operation){
+    Node* buf1, *buf2;
+    buf1 = root->l;
+    buf2 = buf1->r;
+    buf1->r = buf2->l;
+    buf2->l = buf1;
+    buf1->l = buf2->r;
+    buf2->r = root;
+    if (buf2->bal == -1){
+        root->bal = 1;
+    }
+    else{
+        root->bal = 0;
+    }
+    if (buf2->bal == 1){
+        buf1->bal = -1;
+    }else{
+        buf1->bal = 0;
+    }
+    root = buf2;
+    if (operation){
+        root->bal = 0;
+    }else{
+        buf2->bal = 0;
+    }
+}
 
+void RRturn(Node* root, bool operation){
+    Node* buf;
+    int tempBal;
 
+    buf = root->r;
+    root->r = buf->l;
+    buf->l = root;
+    
+    if (operation){
+        root->bal = 0;
+        buf->bal = 0;
+    }
+    else{
+        tempBal = buf->bal;
+        if (tempBal == 0){
+            root->bal = 1;
+            buf->bal = -1;
+        }
+        else{
+            root->bal = 0;
+            buf->bal = 0;
+        }
+    }
+
+    root = buf;
+}
+
+void RLturn(Node* root, bool operatio){
+    
+}
 
 #endif AVL_TREE_H
